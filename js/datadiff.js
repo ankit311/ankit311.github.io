54c8910961f5993e93066edd131f90fb72c92ac5
ANKIT <ankitkr31195@gmail.com>
2019-08-25 00:53:50 +0530

    some useful message

--- js/datadiff.js
+++ js/datadiff.js
@@ -1,6 +1,10 @@
-function datadiff(){
+function datediff(){
     var joiningdate = new Date(2018, 7, 16 , 0, 0, 0, 0);
     var currdate = new Date();
-    return Math.ceil((currdate - joiningdate)/(1000*60*60*24*30*12));
+    var tot_month = Math.ceil((currdate - joiningdate)/(1000*60*60*24*30))
+    var year = Math.floor(tot_month/12);
+    var month = tot_month % 12;
+    return [year, month];
 }
-document.getElementById("diff").innerHTML = "Duration: "+datadiff()+" year";
+var val = datediff();
+document.getElementById("diff").innerHTML = "Duration: "+val[0]+" year "+val[1]+" months";
